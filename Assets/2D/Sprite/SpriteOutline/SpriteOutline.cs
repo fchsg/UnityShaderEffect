b5bbda020a37be25c3024ac0ec6a1338cf459622
fuchenhao <940299880@qq.com>
2019-07-03 09:47:43 +0800

    add sprite outline effect

---
+++ Assets/2D/Sprite/SpriteOutline/SpriteOutline.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+namespace _2D.Sprite.SpriteOutline
+{
+    [AddComponentMenu("Effects/Sprite Outline")]
+    [RequireComponent(typeof(SpriteRenderer)), DisallowMultipleComponent, ExecuteInEditMode]
+    public class SpriteOutline : MonoBehaviour
+    {
+        public SpriteRenderer Renderer { get; private set; }
+
+        private Color GlowColor
+        {
+            get => glowColor;
+            set { if (glowColor != value) { glowColor = value; SetMaterialProperties(); } }
+        }
+
+        private float GlowBrightness
+        {
+            get => glowBrightness;
+            set { if (glowBrightness != value) { glowBrightness = value; SetMaterialProperties(); } }
+        }
+
+        private int OutlineWidth
+        {
+            get => outlineWidth;
+            set { if (outlineWidth != value) { outlineWidth = value; SetMaterialProperties(); } }
+        }
+
+        private float AlphaThreshold
+        {
+            get => alphaThreshold;
+            set { if (alphaThreshold != value) { alphaThreshold = value; SetMaterialProperties(); } }
+        }
+        public bool DrawOutside
+        {
+            get => drawOutside;
+            set { if (drawOutside != value) { drawOutside = value; SetMaterialProperties(); } }
+        }
+        public bool EnableInstancing
+        {
+            get => enableInstancing;
+            set { if (enableInstancing != value) { enableInstancing = value; SetMaterialProperties(); } }
+        }
+
+        [Tooltip("Base color of the glow.")]
+        [SerializeField] private Color glowColor = Color.white;
+        [Tooltip("The brightness (power) of the glow."), Range(1, 10)]
+        [SerializeField] private float glowBrightness = 2f;
+        [Tooltip("Width of the outline, in texels."), Range(0, 10)]
+        [SerializeField] private int outlineWidth = 1;
+        [Tooltip("Threshold to determine sprite borders."), Range(0f, 1f)]
+        [SerializeField] private float alphaThreshold = .01f;
+        [Tooltip("Whether the outline should only be drawn outside of the sprite borders. Make sure sprite texture has sufficient transparent space for the required outline width.")]
+        [SerializeField] private bool drawOutside = false;
+        [Tooltip("Whether to enable GPU instancing.")]
+        [SerializeField] private bool enableInstancing = false;
+
+        private static readonly int IsOutlineEnabledId = Shader.PropertyToID("_IsOutlineEnabled");
+        private static readonly int OutlineColorId = Shader.PropertyToID("_OutlineColor");
+        private static readonly int OutlineSizeId = Shader.PropertyToID("_OutlineSize");
+        private static readonly int AlphaThresholdId = Shader.PropertyToID("_AlphaThreshold");
+
+        private MaterialPropertyBlock _materialProperties;
+
+        private void Awake ()
+        {
+            Renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable ()
+        {
+            SetMaterialProperties();
+        }
+
+        private void OnDisable ()
+        {
+            SetMaterialProperties();
+        }
+
+        private void OnValidate ()
+        {
+            if (!isActiveAndEnabled) return;
+            SetMaterialProperties();
+        }
+
+        private void OnDidApplyAnimationProperties ()
+        {
+            SetMaterialProperties();
+        }
+
+        private void SetMaterialProperties ()
+        {
+            if (!Renderer) return;
+
+            Renderer.sharedMaterial = SpriteOutlineMat.GetSharedFor(this);
+
+            if (_materialProperties == null) // Initializing it at `Awake` or `OnEnable` causes nullref in editor in some cases.
+                _materialProperties = new MaterialPropertyBlock();
+
+            _materialProperties.SetFloat(IsOutlineEnabledId, isActiveAndEnabled ? 1 : 0);
+            _materialProperties.SetColor(OutlineColorId, GlowColor * GlowBrightness);
+            _materialProperties.SetFloat(OutlineSizeId, OutlineWidth);
+            _materialProperties.SetFloat(AlphaThresholdId, AlphaThreshold);
+
+            Renderer.SetPropertyBlock(_materialProperties);
+        }
+    }
+}
---
+++ Assets/2D/Sprite/SpriteOutline/SpriteOutlineMat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _2D.Sprite.SpriteOutline
+{
+    public class SpriteOutlineMat : Material
+    {
+        private Texture SpriteTexture => mainTexture;
+        private bool DrawOutside => IsKeywordEnabled(OutsideMaterialKeyword);
+        private bool InstancingEnabled => enableInstancing;
+
+        private const string OutlineShaderName = "SG/Unlit/Sprite/Outline";
+        private const string OutsideMaterialKeyword = "SPRITE_OUTLINE_OUTSIDE";
+        private static readonly Shader OutlineShader = Shader.Find(OutlineShaderName);
+
+        private static readonly List<SpriteOutlineMat> sharedMaterials = new List<SpriteOutlineMat>();
+
+        private SpriteOutlineMat (Texture spriteTexture, bool drawOutside = false, bool instancingEnabled = false)
+            : base(OutlineShader)
+        {
+            if (!OutlineShader) Debug.LogError($"{OutlineShaderName} shader not found. Make sure the shader is included to the build.");
+
+            mainTexture = spriteTexture;
+            if (drawOutside) EnableKeyword(OutsideMaterialKeyword);
+            if (instancingEnabled) enableInstancing = true;
+        }
+
+        public static Material GetSharedFor (SpriteOutline spriteGlow)
+        {
+            foreach (var t in sharedMaterials)
+            {
+                if (t.SpriteTexture == spriteGlow.Renderer.sprite.texture &&
+                    t.DrawOutside == spriteGlow.DrawOutside &&
+                    t.InstancingEnabled == spriteGlow.EnableInstancing)
+                    return t;
+            }
+
+            var material = new SpriteOutlineMat(spriteGlow.Renderer.sprite.texture, spriteGlow.DrawOutside, spriteGlow.EnableInstancing);
+            material.hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor | HideFlags.NotEditable;
+            sharedMaterials.Add(material);
+
+            return material;
+        }
+    }
+}
