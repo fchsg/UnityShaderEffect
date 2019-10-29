253a9f6e2836c406ec8519a664a00a62e28a0cdb
fchsg <940299880@qq.com>
2019-10-29 22:15:52 +0800

    verticle disslove

---
+++ Assets/VerticalDissolve/Knight/scripts/PingpongDissolve.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace VerticalDissolve.Knight.scripts
+{
+	public class PingpongDissolve : MonoBehaviour {
+
+		// Use this for initialization
+		public float minValue;
+		public float maxValue;
+		public float freq;
+		public Material[] mat;
+		private float fill = 0f;
+		private float range;
+		public float phase = 0f;
+		void Start () {
+			setValue();
+			range = maxValue - minValue;
+		}
+	
+		// Update is called once per frame
+		void Update () {
+			fill = (range / 2f) * (Mathf.Sin(freq * Time.time + phase) + 1f ) + minValue;
+			setValue();
+		}
+
+		private void setValue() {
+			for (int i = 0; i < mat.Length; i++) {
+				mat[i].SetFloat("_Fill", fill);
+			}
+		}
+	}
+}
---
+++ Assets/VerticalDissolve/Knight/scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace VerticalDissolve.Knight.scripts
+{
+	public class Rotation : MonoBehaviour {
+
+		[FormerlySerializedAs("Speed")] public float speed;
+		[FormerlySerializedAs("Direction")] public Vector3 direction = Vector3.zero;
+
+		private void Update () 
+		{
+			transform.Rotate(speed * Time.deltaTime * direction, Space.World);
+		
+		}
+	}
+}
